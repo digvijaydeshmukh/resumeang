7f5a1161bb8378ad47bc2423c63b5243ed9db611
manoj.ambar <manoj.ambar@spheregen.com>
2019-04-19 18:48:45 +0530

    adding new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { CounterComponent } from './counter/counter.component';
+import { PorfolioComponent } from './porfolio/porfolio.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { BlogsComponent } from './blogs/blogs.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component'
+import {FormsModule} from '@angular/forms'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    HomeComponent,
+    ServicesComponent,
+    CounterComponent,
+    PorfolioComponent,
+    TestimonialComponent,
+    BlogsComponent,
+    ContactUsComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path:'',redirectTo:'home',pathMatch:'full'},
+      {path:'home',component:HomeComponent},
+      {path:'about',component:AboutComponent},
+      {path:'service',component:ServicesComponent},
+      {path:'porfolio',component:PorfolioComponent},
+      {path:'blog',component:BlogsComponent},
+      {path:'contactus',component:ContactUsComponent},
+      // {path:'testimonial',component:TestimonialComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/contact-us/contact-us.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-us',
+  templateUrl: './contact-us.component.html',
+  styleUrls: ['./contact-us.component.css']
+})
+export class ContactUsComponent implements OnInit {
+
+  name;
+  email;
+  subject;
+  message;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  save(v){
+    console.log(v)
+  }
+}
